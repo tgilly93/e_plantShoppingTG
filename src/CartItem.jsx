4a61a01f87256e4d7d6698d9b32ffe8d49c33645
Terry Gilmore <tgilly93@gmail.com>
2025-01-21 07:41:54 -0500

    TG ensuring successful Git integration.

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
-import './CartItem.css';
+import './CartItem.css'
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
