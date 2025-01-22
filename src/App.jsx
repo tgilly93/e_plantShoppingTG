e33e21203185a4c780dd8a55d2a12b5e10fae932
Terry Gilmore <tgilly93@gmail.com>
2025-01-22 15:18:04 -0500

    TG implement continue shopping button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
+import CartItem from './CartItem';
 
 function App() {
   
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  const handleContinueShopping = () => {
     setShowProductList(true);
   };
 
@@ -34,6 +39,9 @@ function App() {
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList />
+      </div>
+      <div className={`cart-container ${!showProductList ? 'visible' : ''}`}>
+        <CartItem onContinueShopping={handleContinueShopping} />
       </div>
     </div>
   );
--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -19,7 +19,10 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
+    console.log(e);  
+   if (onContinueShopping) {
+    onContinueShopping(e);
+   }
   };
 
 
@@ -61,7 +64,7 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button" onClick={handleContinueShopping}>Continue Shopping</button>
         <br />
         <button className="get-started-button1">Checkout</button>
       </div>
